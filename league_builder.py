35395118d8fc8d11880f0dc63107491404823a3a
Eddy Hood <eddy@ignitespot.com>
2017-06-24 16:18:19 -0600

    Player sort and assign funct

--- league_builder.py
+++ league_builder.py
@@ -1,4 +1,5 @@
 import csv
+import random
 #Make sure script doesn't execute when imported. Keep all code within name == main
 
 #Create variables and programming logic to divide the 18 players into three teams:
@@ -17,17 +18,47 @@ if __name__ == '__main__':
 
     #open soccer_players.csv with the ability to read and parse data. 
 
-    def open_file():
+    sharks = []
+    dragons = []
+    raptors = []
+
+    def sort_players():
+    	experienced_players = []
+    	new_players = []
+
     	with open("soccer_players.csv", newline = '') as csvfile:
     		reader = csv.DictReader(csvfile, delimiter = ',')
     		for row in reader:
-    			print(row['Name'], row['Soccer Experience'])
+    			if row['Soccer Experience'] == 'YES':
+    				experienced_players.append(row['Name']) 			
+    			else:
+    				new_players.append(row['Name'])
+    		return experienced_players, new_players
 
-    
+    x,y = sort_players()   
+
+   
     #Create logic that seperates the experiences players from the new players
     # def create_roster(Experience):
 
-    
+    def player_picker(players):
+    	#take players and randomly sample and assign them to teams
+    	equal_division = len(players) // 3
+
+    	shark_players = random.sample(players, equal_division)
+    	sharks.append(shark_players)
+
+    	dragon_players = random.sample(players, equal_division)
+    	dragons.append(dragon_players)
+
+    	raptors_players = random.sample(players, equal_division)
+    	raptors.append(raptors_players)
+
+    player_picker(x)
+
+    print('Sharks are: {}'.format(sharks))
+    print('Dragons are: {}'.format(dragons))
+    print('Sharks are: {}'.format(raptors))
 
 
     #Write team name to teams.txt file with seperator line
